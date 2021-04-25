496e1aacc287b7df3fc335f981b3b366c56fb0db
mohammadAqel <mohammedaqel100@gmail.com>
2021-04-26 01:39:12 +0300

    hello

---
+++ my-app/src/Component/HornedBeast.js
@@ -0,0 +1,13 @@
+import React from 'react';
+class HornedBeast extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2> Class 1</h2>
+                <p>HornedBeast </p>
+                <img src="https://www.ifsam.org/newstyle/wp-content/uploads/2020/11/asac.png" alt="asac"></img>
+            </div>
+        )
+    }
+}
+export default HornedBeast;
