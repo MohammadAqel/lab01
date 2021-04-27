615ce3041c0446ff422c48ad175b1e99ea8cbb4b
mohammadAqel <mohammedaqel100@gmail.com>
2021-04-27 18:55:56 +0300

    hello

--- my-app/src/Component/HornedBeast.js
+++ my-app/src/Component/HornedBeast.js
@@ -1,16 +1,38 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 class BeaHornedst extends React.Component {
-   
+    constructor(props) {
+        super(props);
+        this.state = {
+            numOfClike: 0
+        }
+    }
+    suumNumberOfClike = () => {
+        this.setState({ numOfClike: this.state.numOfClike + 1 })
+    }
     render() {
         return (
-            <div>
-                <h1>
-                {this.props.title}
-                </h1>
-                <img src={this.props.imageUrl}alt={this.props.title} title={this.props.title} />
-                <p>
-                {this.props.description}
-                </p>
+            <div>{
+                <Card style={{ width: '18rem', margin: "20px" }}>
+                    <Card.Img variant="top" src={this.props.url} />
+                    <Card.Body>
+                        <Card.Title>
+                            {this.props.description}
+                        </Card.Title>
+
+                        <Card.Text>
+                            {this.props.description}
+                        </Card.Text>
+                        <Card.Text>
+                            {this.state.numOfClike}
+                        </Card.Text>
+                        <Button onClick={this.suumNumberOfClike} variant='primary'>Add Clikcs
+                    </Button>
+                    </Card.Body>
+                </Card>
+            }
+
             </div>
         )
     };
--- my-app/src/Component/main.js
+++ my-app/src/Component/main.js
@@ -1,28 +1,26 @@
-import React from 'react';
+import React, { createFactory } from 'react';
 import HornedBeast from './HornedBeast';
+import arr from './arr';
 class Main extends React.Component {
-    
-    render(){
-        return(
-    <div>
-          <HornedBeast
-        image_url={ "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"}
-        title ={'Rhino Family'}
-        description={ 'A unicorn and a narwhal nuzzling their horns'}/>
-        
-          <HornedBeast
-        image_url={ "https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80"}
-        title ={'UniWhal'}
-        description={ 'Mother (or father) rhino with two babies'}/>
 
-          <HornedBeast
-        image_url={ "https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg"}
-        title ={'Unicorn Head'}
-        description={' Someone wearing a creepy unicorn head mask'}/>
-     
-                
-            </div>
+  render() {
+    return (
+      <div>
+{
+        arr.map((carta) =>{
+        return(
+        <HornedBeast
+          url={carta.image_url}
+          title ={carta.title}
+          description={carta.description}
+          keyword={carta.keyword}
+          horns={'1'}/>
         )
+      })
+
     }
+      </div>
+    )
+  }
 }
 export default Main;
