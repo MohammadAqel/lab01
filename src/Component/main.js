cff67eb12101ba5bc692daa6691a8126f4b00b8e
mohammadAqel <mohammedaqel100@gmail.com>
2021-04-28 15:58:33 +0300

    hello

---
+++ src/Component/main.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import HornedBeast from './HornedBeast';
+import arr from './arr.json';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+class Main extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <Container>
+          <Row>
+            {
+              arr.map((carta) => {
+                return (
+                  <HornedBeast
+                    url={carta.image_url}
+                    title={carta.title}
+                    description={carta.description}
+                    keyword={carta.keyword}
+                    horns={'1'} />
+                );
+              })
+
+            }
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
+export default Main;
