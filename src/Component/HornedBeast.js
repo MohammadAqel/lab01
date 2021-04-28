858727283c6b0c80d9e0bc9dcfaa91d223dbb256
mohammadAqel <mohammedaqel100@gmail.com>
2021-04-29 00:49:25 +0300

    hello

--- src/Component/HornedBeast.js
+++ src/Component/HornedBeast.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
+// import Button from 'react-bootstrap/Button';
+import SelectedBeast from './SelectedBeast';
 class HornedBeast extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      numOfClick: 0
+      numOfClick: 0,
+      show: false
     };
   }
   sumNumberOfClick = () => {
     this.setState({ numOfClick: this.state.numOfClick + 1 });
+
+  }
+  showAnimals = () => {
+    this.setState({ show: true }),
+    this.setState({ numOfClick: this.state.numOfClick + 1 });
+  }
+  stopAnimals = () => {
+    this.setState({ show: false });
+
   }
   render() {
     return (
       <div>{
         <Card style={{ width: '18rem', margin: "20px" }}>
-          <Card.Img variant="top" src={this.props.url} />
+          <Card.Img onClick={this.showAnimals} variant="top" src={this.props.url} />
           <Card.Body>
             <Card.Title>
               {this.props.description}
@@ -27,11 +38,16 @@ class HornedBeast extends React.Component {
             <Card.Text>
               {this.state.numOfClick} ❤️
             </Card.Text>
-            <Button onClick={this.sumNumberOfClick} variant='primary'>Add Click
-            </Button>
           </Card.Body>
         </Card>
       }
+      <SelectedBeast show={this.state.show}
+        handleClose={this.stopAnimals}
+        title={this.props.title}
+        description={this.props.description}
+        url={this.props.url}
+      />
+
 
       </div>
     );
