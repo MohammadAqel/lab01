fe8fb524fe69faef7246506bf74459377c3382dd
mohammadAqel <mohammedaqel100@gmail.com>
2021-05-01 21:26:46 +0300

    hello

--- src/Component/SelectedBeast.js
+++ src/Component/SelectedBeast.js
@@ -1,7 +1,6 @@
-// import { render } from '@testing-library/react';
+import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-import React from 'react';
 class Model extends React.Component {
   render() {
     console.log(this.props);
--- src/Component/header.js
+++ src/Component/header.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import Form from './form';
+
 class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      beasts: ''
+    };
+  }
   render() {
     return (
       <div>
         <header>
-          <h1>Hello From Lab01</h1>
+          <h1>Hello From My Beast Home</h1>
+          <Form
+            filterData={this.props.filterData}
+            numberOfHorns={this.props.numberOfHorns}
+            updateState={this.props.updateState}
+          />
         </header>
       </div>
     );
